b1e430250dd8ddd1eac74d73bc2e5cacfc6c5844
markspcs <markspcs@yahoo.com>
2020-03-16 16:28:51 -0500

    trying to fix heroku2

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
-const mysql = require('mysql');
-
+var mysql = require('mysql');
 var connection;
-//The below for JAWSDB_URL is what I've seen for heroku to work. Testing it. https://blog.edna.tech/2018/08/10/connecting-eat-da-burger-to-jawsdb-on-heroku/
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'test',
+    password: 'cbr954rr',
     database: 'burger_db',
   });
 }
